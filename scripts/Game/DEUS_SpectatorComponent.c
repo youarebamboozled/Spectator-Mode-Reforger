43f078701212fe5e44e18fe025789fdd428c1b1a
youarebamboozled <56235097+youarebamboozled@users.noreply.github.com>
2024-02-27 08:24:10 +0100

    1.0.2

--- scripts/Game/DEUS_SpectatorComponent.c
+++ scripts/Game/DEUS_SpectatorComponent.c
@@ -2,17 +2,220 @@
 class DEUS_SpectatorComponentClass extends ScriptComponentClass { }
 
 class DEUS_SpectatorComponent extends ScriptComponent {
-	SCR_ChimeraCharacter OGEntity;
-	CameraBase OGCamera;
+	private     CameraBase                             	OGCamera;
+	private     CharacterIdentityComponent             	IdentityComponent;
+	private     EquipedLoadoutStorageComponent         	ELSComponent;
+	private     IEntity                                	HeadCoverEntity;
+	private     SCR_CharacterCameraHandlerComponent    	CharCameraHandlerComponent;
+	private		EventHandlerManagerComponent 			EventHandler;
+	// private		CharacterControllerComponent			CharacterController;
 	
-	Animation SpecAnim;
-	TNodeId SpecCamIdx;
+	private     InputManager                           	Input;
 	
-	InputManager Input;
+	private ref ScriptInvoker                          	OnDamageStateChangedSpec;
 	
-	ref ScriptInvoker OnDamageStateChangedSpec;
+	protected   bool                                   	IsSpectating;
+	protected   bool                                   	IsBeingSpectated;
+	private     bool                                   	IsSpectatedInFirstPerson;
 	
-	protected bool IsSpectating;
+	private     vector                                 	CurrentCameraTransform[4];
+	private     vector                                 	TargetCameraTransform[4];
+	
+	private     const float                            	LerpFactor = 0.05;
+	private     float                                  	TargetVerticalFOV;
+	private     float                                  	CurrentVerticalFOV;
+	
+	private		RplId                                  	SpectatorId;	
+	
+	//------------------------------------------------------------------------------------------------
+	void ~DEUS_SpectatorComponent() {
+		OGCamera = null;
+		SpectatedCamera = null;
+		IdentityComponent = null;
+		ELSComponent = null;
+		HeadCoverEntity = null;
+		CharCameraHandlerComponent = null;
+		Input = null;
+		OnDamageStateChangedSpec = null;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+    // Replication Methods
+    //------------------------------------------------------------------------------------------------
+ 	
+	//------------------------------------------------------------------------------------------------
+	private void SetBeingSpectated(IEntity ent, bool state) {
+		Rpc(RpcAsk_SetBeingSpectated, Replication.FindId(ent), state);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
+	private void RpcAsk_SetBeingSpectated(RplId entId, bool state) {
+		IEntity ent = IEntity.Cast(Replication.FindItem(entId));
+		DEUS_SpectatorComponent comp = DEUS_SpectatorComponent.Cast(ent.FindComponent(DEUS_SpectatorComponent));
+		
+		comp.SetBeingSpectatedOwner(Replication.FindId(GetOwner()), state);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	private void SetBeingSpectatedOwner(RplId spectatorId, bool state) {
+		Rpc(RpcDo_SetBeingSpectated, spectatorId, state);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	[RplRpc(RplChannel.Reliable, RplRcver.Owner)]
+	private void RpcDo_SetBeingSpectated(RplId spectatorId, bool state) {
+		IEntity owner = GetOwner();
+		
+		if (state) {
+			IsBeingSpectated = state;
+			SpectatorId = spectatorId;
+			
+			CharCameraHandlerComponent = SCR_CharacterCameraHandlerComponent.Cast(owner.FindComponent(SCR_CharacterCameraHandlerComponent));
+			CharCameraHandlerComponent.GetThirdPersonSwitchInvoker().Insert(OnThirdPersonSwitch);
+			
+			EventHandler = EventHandlerManagerComponent.Cast(owner.FindComponent(EventHandlerManagerComponent));
+			EventHandler.RegisterScriptHandler("OnADSChanged", this, OnADSChanged);
+			
+			Rpc(RpcAsk_OnThirdPersonSwitch, SpectatorId, CharCameraHandlerComponent.IsInThirdPerson());
+			
+			SetEventMask(owner, EntityEvent.POSTFIXEDFRAME);
+		} else {
+			IsBeingSpectated = false;
+			SpectatorId = -1;
+			
+			CharCameraHandlerComponent.GetThirdPersonSwitchInvoker().Remove(OnThirdPersonSwitch);
+			
+			ClearEventMask(owner, EntityEvent.POSTFIXEDFRAME);
+		}
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void OnADSChanged(BaseWeaponComponent weapon, bool inADS) {
+		Rpc(RpcAsk_OnADSChanged, SpectatorId, inADS);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
+	void RpcAsk_OnADSChanged(RplId spectatorId, bool inADS) {
+		IEntity spectator = IEntity.Cast(Replication.FindItem(spectatorId));
+		
+		DEUS_SpectatorComponent comp = DEUS_SpectatorComponent.Cast(spectator.FindComponent(DEUS_SpectatorComponent));
+		
+		comp.OnADSChangedOwner(inADS);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void OnADSChangedOwner(bool inADS) {
+		Rpc(RpcDo_OnADSChanged, inADS);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	[RplRpc(RplChannel.Reliable, RplRcver.Owner)]
+	void RpcDo_OnADSChanged(bool inADS) {
+		if (!IsSpectatedInFirstPerson) {
+			HideHeadAndAccessories(inADS);
+		}
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	private void OnThirdPersonSwitch() {
+    	bool isInThirdPerson = CharCameraHandlerComponent.IsInThirdPerson();
+		Rpc(RpcAsk_OnThirdPersonSwitch, SpectatorId, isInThirdPerson);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
+	private void RpcAsk_OnThirdPersonSwitch(RplId spectatorId, bool isInThirdPerson) {
+		IEntity spectator = IEntity.Cast(Replication.FindItem(spectatorId));
+		
+		DEUS_SpectatorComponent comp = DEUS_SpectatorComponent.Cast(spectator.FindComponent(DEUS_SpectatorComponent));
+		comp.OnThirdPersonSwitchOwner(isInThirdPerson);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	private void OnThirdPersonSwitchOwner(bool isInThirdPerson) {
+		Rpc(RpcDo_OnThirdPersonSwitch, isInThirdPerson);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	[RplRpc(RplChannel.Reliable, RplRcver.Owner)]
+	private void RpcDo_OnThirdPersonSwitch(bool isInThirdPerson) {
+	    IsSpectatedInFirstPerson = !isInThirdPerson;
+	    HideHeadAndAccessories(IsSpectatedInFirstPerson);
+	}	
+	
+	//------------------------------------------------------------------------------------------------
+	CameraBase SpectatedCamera;
+    private void UpdateSpectatorCamera() {
+        if (!IsBeingSpectated) {
+			return;
+		}
+
+        SpectatedCamera = g_ARGame.GetCameraManager().CurrentCamera();
+        if (SpectatedCamera) {
+			vector mat[4];
+            SpectatedCamera.GetTransform(mat);
+			
+            Rpc(RpcAsk_ReceiveCameraData, SpectatorId, mat, SpectatedCamera.GetVerticalFOV());
+        }
+    }
+	
+	//------------------------------------------------------------------------------------------------
+    [RplRpc(RplChannel.Unreliable, RplRcver.Server)]
+    private void RpcAsk_ReceiveCameraData(RplId spectatorId, vector mat[4], float vFOV) {
+		IEntity spectator = IEntity.Cast(Replication.FindItem(spectatorId));
+		DEUS_SpectatorComponent comp = DEUS_SpectatorComponent.Cast(spectator.FindComponent(DEUS_SpectatorComponent));
+		
+		comp.ReceiveCameraDataOwner(mat, vFOV);
+    }
+	
+	//------------------------------------------------------------------------------------------------
+    private void ReceiveCameraDataOwner(vector mat[4], float vFOV) {
+		Rpc(RpcDo_ReceiveCameraData, mat, vFOV);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+    [RplRpc(RplChannel.Unreliable, RplRcver.Owner)]
+    private void RpcDo_ReceiveCameraData(vector mat[4], float vFOV) {
+        TargetCameraTransform = mat;
+		TargetVerticalFOV = vFOV;
+    }    
+   	
+	//------------------------------------------------------------------------------------------------
+    // Event Methods
+    //------------------------------------------------------------------------------------------------
+ 	
+	//------------------------------------------------------------------------------------------------
+	override void EOnPostFrame(IEntity owner, float timeSlice) {
+	    ApplyCameraTransform();;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	override void EOnPostFixedFrame(IEntity owner, float timeSlice) {
+	    UpdateSpectatorCamera();
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	override void EOnFrame(IEntity owner, float timeSlice) {
+	    ApplyCameraTransform();
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void OnEscapeDown() {
+		StopSpectating();
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void OnSpectatedEntityDamageStateChanged(HitZone hz) {
+		if (hz.GetDamageState() == EDamageState.DESTROYED) {
+			StopSpectating();
+		}
+	}
+	
+	//------------------------------------------------------------------------------------------------
+    // Other Methods
+    //------------------------------------------------------------------------------------------------
 	
 	//------------------------------------------------------------------------------------------------
 	void StartSpectating(IEntity ent) {
@@ -31,80 +234,92 @@ class DEUS_SpectatorComponent extends ScriptComponent {
 			}
 		}
 		
-		OGEntity = SCR_ChimeraCharacter.Cast(GetOwner());
+		SCR_ChimeraCharacter OGEntity = SCR_ChimeraCharacter.Cast(GetOwner());
+		
+		/* CharacterController = OGEntity.GetCharacterController();
+		if (CharacterController) {
+			CharacterController.SetDisableViewControls(true);
+			CharacterController.SetDisableWeaponControls(true);
+			CharacterController.SetDisableMovementControls(true);
+		} */
+		
 		OGCamera = g_ARGame.GetCameraManager().CurrentCamera();
 		
-		SetControlledEntity(ent);
+		CurrentVerticalFOV = OGCamera.GetVerticalFOV();
 		
-		SetEventMask(GetOwner(), EntityEvent.POSTFRAME);
+		if (OGCamera) {
+	        OGCamera.GetTransform(CurrentCameraTransform);
+	    }
+		
+		IdentityComponent = CharacterIdentityComponent.Cast(ent.FindComponent(CharacterIdentityComponent));
+		
+		ELSComponent = EquipedLoadoutStorageComponent.Cast(ent.FindComponent(EquipedLoadoutStorageComponent));
+		if (ELSComponent) {
+			HeadCoverEntity = ELSComponent.GetClothFromArea(LoadoutHeadCoverArea);
+		}
+		
+		SetBeingSpectated(ent, true);
+				
+		SetEventMask(GetOwner(), EntityEvent.POSTFRAME | EntityEvent.FRAME);		
 		
 		IsSpectating = true;
 	}
 	
 	//------------------------------------------------------------------------------------------------
-	override void EOnPostFrame(IEntity owner, float timeSlice) {
-		if (OGCamera) {
-			
-		}
-	}
-	
-	//------------------------------------------------------------------------------------------------
-	void SetControlledEntity(IEntity ent) {
-		Rpc(RpcAsk_SetControlledEntity, Replication.FindId(ent), Replication.FindId(g_ARGame.GetPlayerController()));
-	}
-	
-	//------------------------------------------------------------------------------------------------
-	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
-	void RpcAsk_SetControlledEntity(RplId entId, RplId pcId) {
-		IEntity ent = IEntity.Cast(Replication.FindItem(entId));
-		PlayerController pc = PlayerController.Cast(Replication.FindItem(pcId));
-		if (ent && pc) {
-			pc.SetControlledEntity(ent);
-		}
-	}
-	
-	//------------------------------------------------------------------------------------------------
-	void OnSpectatedEntityDamageStateChanged(HitZone hz) {
-		if (hz.GetDamageState() == EDamageState.DESTROYED) {
-			StopSpectating();
-		}
-	}
-	
-	//------------------------------------------------------------------------------------------------
-	void OnEscapeDown() {
-		StopSpectating();
-	}
-	
-	//------------------------------------------------------------------------------------------------
 	void StopSpectating() {
-		if (!Input) {
-			Input = g_Game.GetInputManager();
-		}
-		
 		Input.RemoveActionListener("MenuOpen", EActionTrigger.DOWN, OnEscapeDown);
+		
+		/* if (CharacterController) {
+			CharacterController.SetDisableViewControls(true);
+			CharacterController.SetDisableWeaponControls(true);
+			CharacterController.SetDisableMovementControls(true);
+		} */
+		
+		HideHeadAndAccessories(false);
 		
 		OnDamageStateChangedSpec.Remove(OnSpectatedEntityDamageStateChanged);
 		
-		if (OGEntity.GetDamageManager().GetState() != EDamageState.DESTROYED) {
-			SetControlledEntity(OGEntity);
-		} else {
-			TrySpawnPlayer(g_ARGame.GetPlayerController() ,SCR_SpawnPoint.GetSpawnPoints().GetRandomElement());
-		}
+		ClearEventMask(GetOwner(), EntityEvent.POSTFRAME | EntityEvent.FRAME);	
 		
 		IsSpectating = false;
+	}	 
+	
+	//------------------------------------------------------------------------------------------------
+	private void ApplyCameraTransform() {
+		if (IsSpectating && OGCamera) {
+	       	float adjustedLerpFactor = Math.Clamp(LerpFactor, 0, 1);
+	
+	        CurrentCameraTransform[3] = vector.Lerp(CurrentCameraTransform[3], TargetCameraTransform[3], adjustedLerpFactor);
+			vector angles = vector.Lerp(Math3D.MatrixToAngles(CurrentCameraTransform), Math3D.MatrixToAngles(TargetCameraTransform), adjustedLerpFactor);
+			Math3D.AnglesToMatrix(angles, CurrentCameraTransform);
+			
+	        OGCamera.SetTransform(CurrentCameraTransform);
+			
+			CurrentVerticalFOV = Math.Lerp(CurrentVerticalFOV, TargetVerticalFOV, adjustedLerpFactor);
+			OGCamera.SetVerticalFOV(CurrentVerticalFOV);
+	    } else if (!OGCamera) {
+	        OGCamera = g_ARGame.GetCameraManager().CurrentCamera();
+	    }
 	}
 	
 	//------------------------------------------------------------------------------------------------
-	protected void TrySpawnPlayer(notnull PlayerController playerController, notnull SCR_SpawnPoint spawnPoint){
-		SCR_RespawnComponent spawnComponent = SCR_RespawnComponent.Cast(playerController.FindComponent(SCR_RespawnComponent));
-		SCR_PlayerLoadoutComponent loadoutComponent = SCR_PlayerLoadoutComponent.Cast(playerController.FindComponent(SCR_PlayerLoadoutComponent));
-		
-		SCR_BasePlayerLoadout loadout = loadoutComponent.GetAssignedLoadout();
-		ResourceName loadoutResource = loadout.GetLoadoutResource();
-		SCR_SpawnData spawnData = new SCR_SpawnPointSpawnData(loadoutResource, spawnPoint.GetRplId());		
-		
-		spawnComponent.RequestSpawn(spawnData);
-	}		 
+	private void HideHeadAndAccessories(bool hide = true) {
+		if (hide) {
+			if (HeadCoverEntity) {
+				HeadCoverEntity.ClearFlags(EntityFlags.VISIBLE);
+			}
+			if (IdentityComponent) {
+				IdentityComponent.SetHeadAlpha(255);
+			}
+		} else {
+			if (HeadCoverEntity) {
+				HeadCoverEntity.SetFlags(EntityFlags.VISIBLE);
+			}
+			if (IdentityComponent) {
+				IdentityComponent.SetHeadAlpha(0);
+			}
+		}
+	}
 	
 	//------------------------------------------------------------------------------------------------
 	bool IsCurrentlySpectating() {
